fa91d022446271db04c050483b4305e174b6501a
MrMirror21 <mylittlesentry@gmail.com>
2020-01-18 22:13:40 +0900

    add some css

--- lecture3-1/weather.js
+++ lecture3-1/weather.js
@@ -4,7 +4,7 @@ const COORDS = 'coords';
 
 function getWeather(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    )
+    ) // always use backtick
     .then(function(response){
         return response.json();
     })
